8472b39d4bf2aab1b37e00141978b668b60f747d
vijendra <vijendrakumar5501@gmail.com>
2025-07-22 08:52:39 +0530

    updted

---
+++ src/main/java/com/eNotes/ENotesApplication.java
@@ -0,0 +1,13 @@
+package com.eNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ENotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ENotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eNotes/ENotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
